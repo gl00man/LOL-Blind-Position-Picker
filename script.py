a8ed52ffed35e08fb19fa0f866b99c09c448a6a9
gl00man <60927629+gl00man@users.noreply.github.com>
2021-01-09 19:30:57 +0100

    Script uploaded to repo

---
+++ script.py
@@ -0,0 +1,34 @@
+﻿import pyautogui as pag
+import time
+
+delay = 0.5 #time between next messages
+
+positions = {
+	"A" : "top",
+	"B" : "jungle",
+	"C" : "mid",
+	"D" : "adc",
+	"E" : "supp"
+}
+
+print('Pick a position(pick a letter):')
+print('A. top')
+print('B. jungle')
+print('C. mid')
+print('D. adc')
+print('E. supp')
+
+pos = input('Choice: ')
+pos = pos.upper()
+try:
+	print('Position picked: ' + positions[pos])
+	
+	while True:
+		time.sleep(delay)
+		print('Typing...')
+		pag.typewrite(positions[pos])
+		pag.press("enter")
+	
+except:
+	print('Wrong letter')
+	input('Press enter to exit...')
